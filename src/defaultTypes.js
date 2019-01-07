8ef15f49c9871a7a238dd095fb31860fe1a9ecbe
Colin <colin.diesh@gmail.com>
2019-01-07 12:34:20 -0500

    Add more autosql tests

--- src/defaultTypes.js
+++ src/defaultTypes.js
@@ -10,6 +10,8 @@ const types = {}
   'bigGenePred',
   'bigChain',
   'bigLink',
+  'mafFrames',
+  'mafSummary',
 ].forEach(e => {
   const ret = parser.parse(fs.readFileSync(`./src/as/${e}.as`).toString())
   types[e] = ret
--- test/parse.test.js
+++ test/parse.test.js
@@ -39,3 +39,117 @@ describe('bigGenePred', () => {
     expect(f2).toMatchSnapshot()
   })
 })
+
+describe('bigPsl', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'bigPsl' })
+  })
+  it('bigPsl', () => {
+    const f1 = p.parseLine(
+      'chr1\t11873\t14361\tmAM992877\t1000\t+\t11873\t14361\t0\t3\t354,109,1141,\t0,739,1347,\t0\t1604\t+\t1604\t0,354,463,\t\t\t248956422\t1579\t25\t0\t0\t0',
+    )
+    const f2 = p.parseLine(
+      'chr1\t11873\t14361\tmAM992877\t1000\t+\t12622\t13259\t0\t3\t354,109,1141,\t0,739,1347,\t0\t1604\t+\t1604\t0,354,463,\tcttgccgtcagccttttctttgacctcttctttctgttcatgtgtatctgctgtctcttagcccagacttcccgtgtcctttccaccgggcctttgggaggtcacagggtcttgatgctgtggtcttgatctgcaggtgtctgacttccagcaactgctggcctgtgccagggtggaagctgagcactggagtggagttttcctgtggagaggagccatgcctagagtgggatgggccattgttcatattctggcccctgttgtctgcatgtaacctaataccacgaccaggcatgggggaaagattggaggaaagttgagtgagaggatcaacttctctgacaacctaggccagtgtgtggtgatgccaggcatgcccttccccagcatcaggtctccagagctgcagaagacgacggccgacttggatcacactcttgtgagtgtccccagtgttgcagaggcagggccatcaggcaccaaagggattctgccagcatagtgctcctggaccagtgatacacccggcaccctgtcctggacaggctgttggcctggatctgagccctcgtggaggtcaaagccacctttggttctgccattactgctgtgtggaagttcactcctgccttttcctttcccgagagcctccaccaccccgagatcgcatttctcactgccttttgtctgcccagtttcaccagaagtaggcctcttcctgacaggcagctgcaccactgcctggcgctgcgcccttcctttgctctgcccgctggagacggtgtttgtcatgggcctggtctgcagggatcctgctacaaaggtgaaacccaggagagtgtggagtccagagtgttgccaggacccaggcacaggcattagtgcccgttggagaaaacaggggaatcccgaagaaatggtgggttttggccatccgtgagatcttcccagggcagctcccctctgtggaatccaatctgtcttccatcctgcgtggccgagggccaggcttctcactgggcctctgcaggaggctgccatttgtcctgcccaccgtcttagaagcgagacggagcagactcatctgctactgccctttctataataactaaagttagctgccctggactattcaccccctagtctcaatttaaaaagatccccatggccacagggcccctgcctgggggcttgtcacctcccccaccttcttcctgagtcacttctgcagccttgctccctaacctgccccacagccttgcctggatttctatctccctggcttggtgccagttcctccaagtcgatggcacctccctccctctcaaccacttgagcaaactccaagacatcttctaccccaacaccagcaattgtgccaagggccattaggctctcagcatgactatttttagagaccccgtgtctgtcactgaaaccttttttgtgggagactattcctcccatctgcaacagctgcccctgctgacggcccttctctcctccctctcatcccagagaaacaggtcagctgggagcttctgcccccactgcctagggaccaacaggggcaggaggcagtcactgaccccgagacgtttgca\t365..502\t248956422\t1579\t25\t0\t0\t1',
+    )
+    expect(f1).toMatchSnapshot()
+    expect(f2).toMatchSnapshot()
+  })
+})
+// bigMaf
+//
+describe('bigMaf', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'bigMaf' })
+  })
+  it('bigMaf', () => {
+    const f1 = p.parseLine(
+      'chr22_KI270731v1_random\t0\t2\ta score=60.000000;s hg38.chr22_KI270731v1_random        0 2 +   150754 AC;s canFam3.chr26                27974205 2 + 38964690 AT;',
+    )
+    expect(f1).toMatchSnapshot()
+  })
+})
+
+describe('mafSummary', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'mafSummary' })
+  })
+  it('mafSummary', () => {
+    const f1 = p.parseLine(
+      'chr22_KI270731v1_random\t0\t9270\tcanFam3\t0.568679\t\t',
+    )
+    expect(f1).toMatchSnapshot()
+  })
+})
+
+describe('mafFrames', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'mafFrames' })
+  })
+
+  it('mafFrames', () => {
+    const f1 = p.parseLine(
+      'chr22_KI270731v1_random\t11287\t12574\thg38\t0\t-\tENST00000619792.1\t-1\t-1\t1\t1',
+    )
+    expect(f1).toMatchSnapshot()
+  })
+})
+
+describe('bigInteract', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'bigInteract' })
+  })
+
+  it('bigInteract', () => {
+    const f1 = p.parseLine(
+      'chr3\t63702628\t63705638\t.\t584\t10\t.\t0\tchr17\t58878552\t58880897\t.\t.\tchr3\t63702628\t63705638\t.\t.',
+    )
+    expect(f1).toMatchSnapshot()
+  })
+})
+
+describe('bigChain', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'bigChain' })
+  })
+
+  it('bigChain', () => {
+    const f1 = p.parseLine(
+      'chr22_KI270731v1_random\t133689\t133710\t530549\t1000\t-\t150754\tchr16\t98207768\t80144720\t80144725\t66',
+    )
+    expect(f1).toMatchSnapshot()
+  })
+})
+
+describe('bigLink', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'bigLink' })
+  })
+
+  it('bigLink', () => {
+    const f1 = p.parseLine(
+      'chr22_KI270731v1_random\t7917\t7918\t2912\t75185566',
+    )
+    expect(f1).toMatchSnapshot()
+  })
+})
+
+describe('bigNarrowPeak', () => {
+  let p
+  beforeAll(() => {
+    p = new BED({ type: 'bigNarrowPeak' })
+  })
+
+  it('bigNarrowPeak', () => {
+    const f1 = p.parseLine(
+      'chr1\t566753\t566953\t.\t468\t.\t103.84\t5.54347\t4.80079\t154',
+    )
+    expect(f1).toMatchSnapshot()
+  })
+})
