35c724554f972b0b1274ecabf4873e5687fe1051
Colin <colin.diesh@gmail.com>
2019-04-02 17:54:07 -0400

    Use webpack in library build mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+var path = require('path');
+
 module.exports = {
+  output: {
+		path: path.resolve(__dirname, 'dist'),
+    library: 'BED',
+    libraryTarget: 'commonjs'
+	},
   module: {
     rules: [
       {
