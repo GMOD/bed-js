a091b032458023ceff362801a73b6bfb94e45d95
Colin <colin.diesh@gmail.com>
2021-08-20 21:15:28 -0400

    Shakeup the config

--- src/defaultTypes.js
+++ src/defaultTypes.js
@@ -1,27 +1,6 @@
-import parser from './autoSql'
-import bigInteract from './as/bigInteract.as'
-import bigMaf from './as/bigMaf.as'
-import bigPsl from './as/bigPsl.as'
-import bigNarrowPeak from './as/bigNarrowPeak.as'
-import bigGenePred from './as/bigGenePred.as'
-import bigLink from './as/bigLink.as'
-import bigChain from './as/bigChain.as'
-import mafFrames from './as/mafFrames.as'
-import mafSummary from './as/mafSummary.as'
-import defaultBedSchema from './as/defaultBedSchema.as'
+import parser from './index'
+import * as types from './as/autoSqlSchemas'
 
-const types = {
-  bigInteract,
-  bigMaf,
-  bigPsl,
-  bigNarrowPeak,
-  bigGenePred,
-  bigLink,
-  bigChain,
-  mafFrames,
-  mafSummary,
-  defaultBedSchema,
-}
 Object.keys(types).forEach((k) => {
   types[k] = parser.parse(types[k].trim())
 })
